8fac52d4775360ae0437bc3c49cb1f7f9bdf139d
alestori7@gmail.com <alestori7@gmail.com>
2021-09-20 10:36:53 -0300

    exercise 6 - subquery

--- SQL/21.2/ex6.sql
+++ SQL/21.2/ex6.sql
@@ -2,6 +2,8 @@ use Pixar;
 
 select * from BoxOffice;
 
+
+-- subquery
 SELECT 
     title
 FROM
@@ -13,3 +15,14 @@ WHERE
             BoxOffice
         WHERE
             rating > 7.5);
+            
+-- inner join
+
+SELECT 
+    m.title
+FROM
+    Movies m
+        JOIN
+    BoxOffice b
+WHERE
+    b.movie_id = m.id AND b.rating > 7.5;
